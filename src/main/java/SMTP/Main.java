f0b8df426a97ccfa8575c546234caf4f7e05547e
pauprats2002 <77688104+pauprats2002@users.noreply.github.com>
2022-02-08 20:18:00 +0100

    LogIn solucionado

--- src/main/java/SMTP/Main.java
+++ src/main/java/SMTP/Main.java
@@ -5,15 +5,19 @@
  */
 package SMTP;
 
+import SMTP.Dialogs.LogInDialog;
+import java.util.regex.Pattern;
+import javax.swing.JOptionPane;
+
+
 /**
  *
  * @author Alumne
  */
 public class Main extends javax.swing.JFrame {
+    
+    private boolean loggedIn = false;
 
-    /**
-     * Creates new form Main
-     */
     public Main() {
         initComponents();
     }
@@ -27,22 +31,131 @@ public class Main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        txtEmail = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jSeparator1 = new javax.swing.JSeparator();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txtAreaMissatge = new javax.swing.JTextArea();
+        jLabel3 = new javax.swing.JLabel();
+        txtAsunto = new javax.swing.JTextField();
+        jSeparator2 = new javax.swing.JSeparator();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        txtEmail.setFont(new java.awt.Font("Roboto Light", 2, 12)); // NOI18N
+        txtEmail.setBorder(null);
+        txtEmail.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                txtEmailMouseClicked(evt);
+            }
+        });
+        txtEmail.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtEmailActionPerformed(evt);
+            }
+        });
+        jPanel1.add(txtEmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 60, 290, 30));
+
+        jLabel1.setFont(new java.awt.Font("Dialog", 3, 12)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel1.setText("Correo Receptor:");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 30, 100, 30));
+
+        jLabel2.setFont(new java.awt.Font("Dialog", 3, 12)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel2.setText("Missatge:");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 200, 60, 30));
+
+        jButton1.setBackground(new java.awt.Color(0, 134, 190));
+        jButton1.setFont(new java.awt.Font("Dialog", 3, 12)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("ENVIAR");
+        jButton1.setToolTipText("");
+        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 420, 120, 30));
+        jPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 90, 290, 20));
+
+        txtAreaMissatge.setColumns(20);
+        txtAreaMissatge.setFont(new java.awt.Font("Roboto Light", 2, 12)); // NOI18N
+        txtAreaMissatge.setLineWrap(true);
+        txtAreaMissatge.setRows(5);
+        txtAreaMissatge.setWrapStyleWord(true);
+        txtAreaMissatge.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.LOWERED));
+        txtAreaMissatge.setOpaque(false);
+        jScrollPane1.setViewportView(txtAreaMissatge);
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 230, 290, 170));
+
+        jLabel3.setFont(new java.awt.Font("Dialog", 3, 12)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel3.setText("Asunto:");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 120, -1, -1));
+
+        txtAsunto.setFont(new java.awt.Font("Roboto Light", 2, 12)); // NOI18N
+        txtAsunto.setBorder(null);
+        jPanel1.add(txtAsunto, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 140, 290, 30));
+        jPanel1.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 170, 290, 10));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void txtEmailActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtEmailActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtEmailActionPerformed
+
+    private void txtEmailMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtEmailMouseClicked
+
+    }//GEN-LAST:event_txtEmailMouseClicked
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String correoReceptor = txtEmail.getText();
+        String asunto = txtAsunto.getText();
+        String missatge = txtAreaMissatge.getText();
+        Pattern emailRegEx = Pattern.compile("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$");
+        if (emailRegEx.matcher(correoReceptor).matches()) {
+
+            LogInDialog ld = new LogInDialog(this, true);
+            ld.setReceptor(correoReceptor);
+            ld.setAsunto(asunto);
+            ld.setMissatge(missatge);
+            ld.setVisible(true);
+
+        } else if (!emailRegEx.matcher(correoReceptor).matches()) {
+            JOptionPane.showMessageDialog(null, "El text introduit no es un correu electronic");
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+           askLoginLoop();
+    }//GEN-LAST:event_formWindowOpened
+  
     /**
      * @param args the command line arguments
      */
@@ -79,5 +192,32 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JTextArea txtAreaMissatge;
+    private javax.swing.JTextField txtAsunto;
+    private javax.swing.JTextField txtEmail;
     // End of variables declaration//GEN-END:variables
+  
+    private void askLoginLoop() {
+        while (!loggedIn) {
+            LogInDialog ld = new LogInDialog(this, true);
+            ld.setVisible(true);
+        }
+    }
+    
+    private void logout() {
+        loggedIn = false;
+        askLoginLoop();
+    }
+    
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
 }
