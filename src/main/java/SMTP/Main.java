aeae689d56e164547a1ef719135bb5ff307e166a
joanandreu99 <joanandreumatas@paucasesnovescifp.cat>
2022-02-11 00:55:13 +0100

    Canvis

--- src/main/java/SMTP/Main.java
+++ src/main/java/SMTP/Main.java
@@ -5,19 +5,32 @@
  */
 package SMTP;
 
-
 import SMTP.Dialogs.LogInDialog;
 import SMTP.Dialogs.SendAgain;
 import java.awt.Frame;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.imageio.ImageIO;
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 /**
@@ -25,9 +38,13 @@ import javax.swing.JOptionPane;
  * @author Alumne
  */
 public class Main extends javax.swing.JFrame {
-    
+
     private boolean loggedIn = false;
-  
+    private LogInDialog logIn = new LogInDialog((Frame) this.getParent(), true);
+    JFileChooser fileChooser;
+    String archivo;
+    String nombre;
+
     public Main() {
         initComponents();
     }
@@ -52,15 +69,12 @@ public class Main extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         txtAsunto = new javax.swing.JTextField();
         jSeparator2 = new javax.swing.JSeparator();
+        jButton2 = new javax.swing.JButton();
+        lblImage = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setLocation(new java.awt.Point(650, 300));
         setResizable(false);
-        addWindowListener(new java.awt.event.WindowAdapter() {
-            public void windowOpened(java.awt.event.WindowEvent evt) {
-                formWindowOpened(evt);
-            }
-        });
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -100,7 +114,7 @@ public class Main extends javax.swing.JFrame {
                 jButton1ActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 420, 120, 30));
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 420, 120, 30));
         jPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 90, 290, 20));
 
         txtAreaMissatge.setColumns(20);
@@ -124,11 +138,24 @@ public class Main extends javax.swing.JFrame {
         jPanel1.add(txtAsunto, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 140, 290, 30));
         jPanel1.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 170, 290, 10));
 
+        jButton2.setBackground(new java.awt.Color(0, 134, 190));
+        jButton2.setFont(new java.awt.Font("Dialog", 3, 12)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(255, 255, 255));
+        jButton2.setText("INSERTAR IMATGE");
+        jButton2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 420, 150, 30));
+        jPanel1.add(lblImage, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 60, 230, 340));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 520, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 592, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -143,61 +170,101 @@ public class Main extends javax.swing.JFrame {
     }//GEN-LAST:event_txtEmailActionPerformed
 
     private void txtEmailMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtEmailMouseClicked
-        
+
     }//GEN-LAST:event_txtEmailMouseClicked
 
+    private ImageIcon resizeImageIcon(BufferedImage originalImage, int desiredWidth, int desiredHeight) {
+        int newHeight = 0;
+        int newWidth = 0;
+        float aspectRatio = (float) originalImage.getWidth() / originalImage.getHeight();
+        if (originalImage.getWidth() > originalImage.getHeight()) {
+            newWidth = desiredWidth;
+            newHeight = Math.round(desiredWidth / aspectRatio);
+        } else {
+            newHeight = desiredHeight;
+            newWidth = Math.round(desiredHeight * aspectRatio);
+        }
+        Image resultingImage = originalImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+        BufferedImage outputImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+        outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
+        ImageIcon imageIcon = new ImageIcon(outputImage);
+        return imageIcon;
+    }
+
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-       
-        String correoRemitente = "interficies99@gmail.com";
-        String passwordRemitente = "1234joan";
+
+        String correoRemitente = logIn.getTxtEmail().getText();
+        String passwordRemitente = logIn.getPswPassword().getText();
         String correoReceptor = txtEmail.getText();
         String asunto = txtAsunto.getText();
         String missatge = txtAreaMissatge.getText();
         Pattern emailRegEx = Pattern.compile("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$");
-                if (emailRegEx.matcher(correoReceptor).matches()) {
-                    Properties props = System.getProperties();
-                    props.put("mail.smtp.ssl.trust", "smtp.gmail.com");  //El servidor SMTP de Google
-                    props.put("mail.smtp.user", correoRemitente);     // El correo del remitente
-                    props.put("mail.smtp.clave", passwordRemitente);  //La clave de la cuenta
-                    props.put("mail.smtp.auth", "true");    //Usar autenticación mediante usuario y clave
-                    props.put("mail.smtp.starttls.enable", "true"); //Para conectar de manera segura al servidor SMTP
-                    props.put("mail.smtp.port", "587"); //El puerto SMTP seguro de Google
-                    System.setProperty("mail.smtp.ssl.protocols", "TLSv1.2"); //Solucionar el error de protocol
+        if (emailRegEx.matcher(correoReceptor).matches()) {
+            Properties props = System.getProperties();
+            props.put("mail.smtp.ssl.trust", "smtp.gmail.com");  //El servidor SMTP de Google
+            props.put("mail.smtp.user", correoRemitente);     // El correo del remitente
+            props.put("mail.smtp.clave", passwordRemitente);  //La clave de la cuenta
+            props.put("mail.smtp.auth", "true");    //Usar autenticación mediante usuario y clave
+            props.put("mail.smtp.starttls.enable", "true"); //Para conectar de manera segura al servidor SMTP
+            props.put("mail.smtp.port", "587"); //El puerto SMTP seguro de Google
+            System.setProperty("mail.smtp.ssl.protocols", "TLSv1.2"); //Solucionar el error de protocol
 
-                    Session session = Session.getDefaultInstance(props);
-                    MimeMessage message = new MimeMessage(session);
+            Session session = Session.getDefaultInstance(props);
+            MimeMessage message = new MimeMessage(session);
+            try {
+                BodyPart texto = new MimeBodyPart();
+                BodyPart adjunto = new MimeBodyPart();
+                adjunto.setDataHandler(new DataHandler(new FileDataSource(archivo)));
+                adjunto.setFileName(nombre);
+                MimeMultipart multiParte = new MimeMultipart();
+                multiParte.addBodyPart(texto);
+                multiParte.addBodyPart(adjunto);
 
-                    try {
+                message.setFrom(new InternetAddress(correoRemitente));
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(correoReceptor));
+                message.setSubject(asunto);
+                message.setText(missatge);
+                message.setText(lblImage.getText());
+                message.setContent(multiParte);
 
-                        message.setFrom(new InternetAddress(correoRemitente));
-                        message.addRecipient(Message.RecipientType.TO, new InternetAddress(correoReceptor));
-                        message.setSubject(asunto);
-                        message.setText(missatge);
+                Transport transport = session.getTransport("smtp");
+                transport.connect("smtp.gmail.com", correoRemitente, passwordRemitente);
+                transport.sendMessage(message, message.getAllRecipients());
+                transport.close();
 
-                        Transport transport = session.getTransport("smtp");
-                        transport.connect("smtp.gmail.com", correoRemitente, passwordRemitente);
-                        transport.sendMessage(message, message.getAllRecipients());
-                        transport.close();
+                JOptionPane.showMessageDialog(null, "Correu Electronic Enviat");
+                setVisible(false);
+                SendAgain sa = new SendAgain((Frame) this.getParent(), true);
+                sa.setVisible(true);
+            } catch (AddressException ex) {
+                ex.printStackTrace();
+            } catch (MessagingException ex) {
+                ex.printStackTrace();
+            }
 
-                        JOptionPane.showMessageDialog(null, "Correu Electronic Enviat");
-                        setVisible(false);
-                        SendAgain sa = new SendAgain((Frame) this.getParent(), true);
-                        sa.setVisible(true);
-                    } catch (AddressException ex) {
-                        ex.printStackTrace();
-                    } catch (MessagingException ex) {
-                        ex.printStackTrace();
-                    }
-                    
-                    
-                } else if (!emailRegEx.matcher(correoReceptor).matches()) {
-                    JOptionPane.showMessageDialog(null, "El text introduit no es un correu electronic");
-                }
+        } else if (!emailRegEx.matcher(correoReceptor).matches()) {
+            JOptionPane.showMessageDialog(null, "El text introduit no es un correu electronic");
+        }
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
         askLoginLoop();
     }//GEN-LAST:event_formWindowOpened
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        fileChooser = new JFileChooser();
+        int returnOption = fileChooser.showOpenDialog(this);
+        if (returnOption == JFileChooser.APPROVE_OPTION)
+            try {
+            BufferedImage bufferedImage = ImageIO.read(new File(fileChooser.getSelectedFile().getAbsolutePath()));
+            ImageIcon icon = resizeImageIcon(bufferedImage, lblImage.getWidth(), lblImage.getHeight());
+            lblImage.setIcon(icon);
+            archivo = fileChooser.getSelectedFile().getPath();
+            nombre = fileChooser.getSelectedFile().getName();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -236,6 +303,7 @@ public class Main extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -243,6 +311,7 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JLabel lblImage;
     private javax.swing.JTextArea txtAreaMissatge;
     private javax.swing.JTextField txtAsunto;
     private javax.swing.JTextField txtEmail;
